9ba3dfe58b0e6e89ea5a08bcbd9b1d2eea67c677
hadyanadam <hadyanadamn@gmail.com>
2020-03-29 18:10:10 +0700

    init

---
+++ leads/tests.py
@@ -0,0 +1,14 @@
+# from django.test import TestCase
+
+# class SomeModelModelTest(TestCase):
+#     def setUp(self):
+#         SomeModel.objects.create(
+#             name=fake.name(),
+#             email=fake.email(),
+#             phone=fake.phone_number(),
+#             message=fake.text(),
+#             source=fake.url()
+#         )
+#     def test_save_model(self):
+#         saved_models = SomeModel.objects.count()
+#         self.assertEqual(saved_models, 2)
