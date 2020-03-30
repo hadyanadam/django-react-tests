0ca64ceb93f4854fcf34d4156a781b652f21445b
hadyanadam <hadyanadamn@gmail.com>
2020-03-30 08:34:03 +0700

    Added Bootstrap Navbar

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import { Container } from "react-bootstrap"
-import Navbar from "./Navbar"
+// import { Container, Navbar, Nav } from "react-bootstrap"
+import 'bootstrap'
+import "./../../static/css/bootstrap.css"
+import "./../../static/css/styles.css"
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -34,8 +37,39 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar />
+    <div className="container-full">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="#">Action</a>
+                  <a className="dropdown-item" href="#">Another action</a>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      <div className="container">
         <ul>
           {this.state.data.map(contact => {
             return (
@@ -45,7 +79,9 @@ class App extends Component {
             );
           })}
         </ul>
+
       </div>
+    </div>
     );
   }
 }
